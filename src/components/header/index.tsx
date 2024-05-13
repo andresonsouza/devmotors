43039ae898bd53c9fcab554e2da3cef08fee033f
Andreson Souza <andresonjazz@gmail.com>
2024-05-13 14:53:25 -0300

    adicione blur e sombreamento ao header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,54 @@
-import styles from "./styles.module.scss";
+"use client"
+
+import { useState, useEffect } from 'react';
+import styles from './styles.module.scss';
+import Link from 'next/link'
 
 export function Header() {
-    return (
-        <header>
-            <h2>HEADER</h2>
-        </header>
-    )
+  const [top, setTop] = useState(true);
+
+  const scrollHandler = () => {
+    window.scrollY > 10 ? setTop(false) : setTop(true);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollHandler);
+
+    // remove o evento ao sair da página
+    return () => window.removeEventListener("scroll", scrollHandler);
+  },
+    [top]);
+
+
+  return (
+    <header className={`${styles.header} ${!top ? styles.fixed : styles.background}`}>
+
+      <div className={styles.container}>
+
+        <div className={styles.content}>
+
+          <div className={styles.contentLogo}>
+            <Link href="/">
+              Dev Motors
+            </Link>
+          </div>
+
+          <nav className={styles.nav}>
+            <Link href="/">
+              HOME
+            </Link>
+
+            <Link href="/#servicos">
+              SERVIÇOS
+            </Link>
+
+            <Link href="/#contatos">
+              HOME
+            </Link>
+          </nav>
+
+        </div>
+      </div>
+    </header>
+  )
 }
